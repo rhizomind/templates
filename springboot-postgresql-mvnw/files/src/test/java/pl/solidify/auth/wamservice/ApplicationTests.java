24e23f6f316ce6a971aba5237eeea6fe20bd0172
Tomasz Krzyżak <tomasz.krzyzak@solidify.pl>
2022-09-28 23:51:58 +0200

    add first template: springboot, maven, postgres

---
+++ springboot-postgresql-mvnw/files/src/test/java/pl/solidify/auth/wamservice/ApplicationTests.java
@@ -0,0 +1,15 @@
+package pl.solidify.auth.wamservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest()
+@ActiveProfiles("integration")
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
